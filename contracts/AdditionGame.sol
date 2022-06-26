47f4b24a0c9ad71bac56bda80d6b32df3b4730e6
octo <dlqhdgkr456@gmail.com>
2022-06-26 20:00:16 +0900

    add gitignore

--- contracts/AdditionGame.sol
+++ contracts/AdditionGame.sol
@@ -1,23 +1,24 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
 
 contract AdditionGame {
     address public owner;
 
-    constructor() public {
+    constructor() {
         owner = msg.sender;
     }
-    
-    function getBalance() public view returns (uint) {
+
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    function deposit() public payable {  
+    function deposit() public payable {
         require(msg.sender == owner);
-    }   
-  
-    function transfer(uint _value) public returns (bool) {
+    }
+
+    function transfer(uint256 _value) public returns (bool) {
         require(getBalance() >= _value);
-        msg.sender.transfer(_value);
+        payable(msg.sender).transfer(_value);
         return true;
     }
 }
