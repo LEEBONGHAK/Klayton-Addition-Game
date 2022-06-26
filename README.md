a77b1fecb5af46d39ef9715df466c3b67e95fe8e
octo <dlqhdgkr456@gmail.com>
2022-06-26 22:31:55 +0900

    deploy contract to baobab network

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Klayton-Addition-Game
+
+## Deploy Smart Contract to Klaytn Network
+  
+```
+> npm install
+> truffle deploy --network klaytn
+```
+  
+## Redeploy Smart Contract to Klaytn Network
+  
+```
+> truffle deploy --compile-all --reset --network klaytn
+```
+- `--compile-all`: re-compile all contracts
+- `--reset`: restart script file in migrations repository by force
--- truffle.js
+++ truffle.js
@@ -1,5 +1,18 @@
 // truffle.js config for klaytn.
-const PrivateKeyConnector = repuire('connect-privatekey-to-provider');
+require('dotenv').config();
+const PrivateKeyConnector = require('connect-privkey-to-provider');
 const NETWORK_ID = '1001';	// Baobab 고유의 네트워크 아이디
 const GASLIMIT = '20000000';	// 배포하는 데 들어가는 가스 한도
-const URL = 'https://api.baobab.klaytn.net:8651'	// 클레이튼의 풀노드가 돌아가고 있는 baobab 테스트넷 rul 
+const URL = 'https://api.baobab.klaytn.net:8651'	// 클레이튼의 풀노드가 돌아가고 있는 baobab 테스트넷
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+	networks: {
+		klaytn: {
+			provider: new PrivateKeyConnector(PRIVATE_KEY, URL),
+			network_id: NETWORK_ID,
+			gas: GASLIMIT,
+			gasPrice: null,
+		}
+	},
+}
