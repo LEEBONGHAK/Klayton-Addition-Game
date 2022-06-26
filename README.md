aaf490a426de8be3f7d51346b177d3253378d42a
octo <dlqhdgkr456@gmail.com>
2022-06-27 02:10:45 +0900

    Made Account Verification UI and fixed webpack.config.js

--- README.md
+++ README.md
@@ -12,5 +12,10 @@
 ```
 > truffle deploy --compile-all --reset --network klaytn
 ```
-- `--compile-all`: re-compile all contracts
-- `--reset`: restart script file in migrations repository by force
+- `--compile-all`: re-compile all contracts  
+- `--reset`: restart script file in migrations repository by force  
+  
+  
+  
+> 참고 자료
+"SEJONG IT EDU 강사님", 인프런 [Klaytn 클레이튼 블록체인 어플리케이션 만들기 - 이론과 실습](https://www.inflearn.com/course/%ED%81%B4%EB%A0%88%EC%9D%B4%ED%8A%BC/dashboard)
--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,23 @@ module.exports = {
   mode: 'development',
   output: {
     filename: "index.js",
-    path: path.resolve(__dirname, 'dist')   
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [   
     // new webpack.DefinePlugin({
     //   DEPLOYED_ADDRESS: JSON.stringify(fs.readFileSync('deployedAddress', 'utf8').replace(/\n|\r/g, "")),
     //   DEPLOYED_ABI: fs.existsSync('deployedABI') && fs.readFileSync('deployedABI', 'utf8'),
     // }),
-    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html"}])
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./src/index.html", to: "index.html" },
+      ],
+    }),
   ],
-  devServer: { contentBase: path.join(__dirname, "dist"), compress: true }
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+  },
 }
