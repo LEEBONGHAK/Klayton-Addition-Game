a0b4dd6c65e06b30316c428cde3d0d58054c5b69
octo <dlqhdgkr456@gmail.com>
2022-06-27 01:01:35 +0900

    fixed package.json

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,11 +6,11 @@ module.exports = function (deployer) {
     // AdditionGame contract deploy
     deployer.deploy(AdditionGame)
         .then(() => {
-            if (AdditionGame.json) {
+            if (AdditionGame._json) {
                 // deployedABI에 json으로 받은 ABI 정보를 작성
                 // ABI: 컨트랙트 함수와 매개 변수들을 JSON 형식으로 나타낸 리스트
                 // ABI는 인터페이스 역할을 하며, 컨트랙트 내의 함수를 호출하거나 컨트랙트로부터 데이터를 얻는 방법
-                fs.writeFile('deployedABI', JSON.stringify(AdditionGame.json.abi),
+                fs.writeFile('deployedABI', JSON.stringify(AdditionGame._json.abi),
                     (err)=> {
                         if (err) throw err;
                         console.log("파일에 ABI 입력 성공")
--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 // truffle.js config for klaytn.
 require('dotenv').config();
-const PrivateKeyConnector = require('connect-privkey-to-provider');
+const PrivateKeyConnector = require('truffle-hdwallet-provider-klaytn');
 const NETWORK_ID = '1001';	// Baobab 고유의 네트워크 아이디
 const GASLIMIT = '20000000';	// 배포하는 데 들어가는 가스 한도
 const URL = 'https://api.baobab.klaytn.net:8651'	// 클레이튼의 풀노드가 돌아가고 있는 baobab 테스트넷
@@ -13,6 +13,11 @@ module.exports = {
 			network_id: NETWORK_ID,
 			gas: GASLIMIT,
 			gasPrice: null,
+		}
+	},
+	compilers: {
+		solc: {
+			version: "0.8.13",      // Fetch exact version from solc-bin (default: truffle's version)
 		}
 	},
 }
