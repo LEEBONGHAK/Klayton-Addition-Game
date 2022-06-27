98639a8d1f6f9496828fdcaf16e06306d0dbe22f
octo <dlqhdgkr456@gmail.com>
2022-06-27 17:03:52 +0900

    keystore validation

--- README.md
+++ README.md
@@ -3,17 +3,23 @@
 ## Deploy Smart Contract to Klaytn Network
   
 ```
-> npm install
-> truffle deploy --network klaytn
+$ npm install
+$ truffle deploy --network klaytn
 ```
   
 ## Redeploy Smart Contract to Klaytn Network
   
 ```
-> truffle deploy --compile-all --reset --network klaytn
+$ truffle deploy --compile-all --reset --network klaytn
 ```
 - `--compile-all`: re-compile all contracts  
 - `--reset`: restart script file in migrations repository by force  
+  
+## Run Webpack Dev Server
+  
+```
+$ npm run dev
+```
   
   
   
--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
+// 클레이튼 블록체인과 소통할 수 있는 라이브러리
+import Caver from 'caver-js';
+
+// 환경설정 변수
+// rpcURL: 어떤 클레이트 노드에 연결해서 사용할지 정의(여기에서는 baobab 테스트넷)
+const config = {
+  rpcURL: 'https://api.baobab.klaytn.net:8651'
+}
+
+// Bapp에서 사용할 수 있도록 rpcURL을 Caver 생성자에 넘겨 인스턴스화
+const cav = new Caver(config.rpcURL);
 
 const App = {
+  // 전역 변수: 접근방법 지정, keystore 전체 내용 및 비밀번호 저장
+  auth: {
+    accessType: 'keystore',
+    keystore: '',
+    password: ''
+  },
 
   start: async function () {
 
   },
 
+  // 유효한 keystore 파일인지 확인 및 내용 가져오기
   handleImport: async function () {
-
+    const fileReader = new fileReader();
+    fileReader.readAsText(event.target.files[0]);
+    fileReader.onload = (event) => {
+      try {
+        if (!this.checkValidKeystore(event.target.result)) {
+          $('#message').text('유효하지 않은 keystore 파일입니다.')  // 에러 메세지
+          return;
+        }
+        this.auth.keystore = event.target.result;
+        $('#message').text('keystore 통과, 비밀번호를 입력하세요.') // 성공 메세지
+        document.querySelector('#input-password').focus();  // password 필드 포커싱
+      } catch (event) {
+        $('#message').text('유효하지 않은 keystore 파일입니다.')  // 에러 메세지
+        return;
+      }
+    }
   },
 
+  // 입력한 비밀번호 가져오기
   handlePassword: async function () {
-
+    this.auth.password = event.target.value;
   },
 
   handleLogin: async function () {
@@ -45,8 +79,15 @@ const App = {
 
   },
 
+  // keystore 파일 유효성 검사
   checkValidKeystore: function (keystore) {
+    const parsedKeystore = JSON.parse(keystore); // keystore 파일 내 내용 분해, 오브젝트 변환 후 저장
+    const isValidKeystore = parsedKeystore.version && 
+      parsedKeystore.id && 
+      parsedKeystore.address && 
+      parsedKeystore.keyring;
 
+    return isValidKeystore;
   },
 
   integrateWallet: function (privateKey) {
